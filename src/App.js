a939e45a4104458436b9cfe58fb0bd41a34576e5
pista33 <yukimori0303@gmail.com>
2023-07-19 15:42:21 +0900

    committest y

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> こんばんは.
+          Edit <code>src/App.js</code> こにちは.
         </p>
         <a
           className="App-link"
