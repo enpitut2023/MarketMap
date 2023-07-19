ee6b2b18b46ff1f75fc3821ec3b613b9670e0869
hiroto-sdch <hiroto.sdchakqjt@gmail.com>
2023-07-19 15:59:02 +0900

    Hello commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> こんちゃ
+          Edit <code>src/App.js</code> Hello
         </p>
         <a
           className="App-link"
